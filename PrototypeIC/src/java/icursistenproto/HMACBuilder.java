de1b1bd2a10e2921b0e3b3fa3d7b3bfce333b4cd
BoumansRaf <raf.boumans@gmail.com>
2017-04-25 14:39:47 +0200

    Hey Boo

--- PrototypeIC/src/java/icursistenproto/HMACBuilder.java
+++ PrototypeIC/src/java/icursistenproto/HMACBuilder.java
@@ -5,6 +5,9 @@
  */
 package icursistenproto;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.nio.charset.Charset;
 import java.security.NoSuchAlgorithmException;
 import javax.crypto.Mac;
@@ -16,10 +19,12 @@ import javax.crypto.spec.SecretKeySpec;
  */
 public class HMACBuilder {
     
-    public String buildHMAC(String verb, String dateTime, String url, String instellingsNr) throws NoSuchAlgorithmException{
+    public String buildHMAC(String verb, String dateTime, String url, String instellingsNr) throws NoSuchAlgorithmException, FileNotFoundException{
         String hmacString = "verb=" + verb + "&timestamp=" + dateTime + "&url=" + url + "&instellingsnr=" + instellingsNr;
         final Mac hmac = Mac.getInstance("HmacSHA256");
-        final Charset ascii = Charset.forName("US-ASCII");
+        final Charset ascii = Charset.forName("UTF-8");
+        BufferedReader read = new BufferedReader(new FileReader("C:\\Users\\Raf\\Documents\\Project\\keys\\private.txt"));
+        
         final SecretKeySpec secret_key = new javax.crypto.spec.SecretKeySpec(ascii.encode("key").array(), "HmacSHA256");
         final byte[] mac_data = hmac.doFinal(ascii.encode(hmacString).array());
         String result = "";
